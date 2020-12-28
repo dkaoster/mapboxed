5259752a0862756db6da35d14588b4d0d662f4b9
Daniel Kao <dkao@diplateevo.com>
2020-12-28 18:06:47 +0800

    ValidateParams

--- src/validateParams.js
+++ src/validateParams.js
@@ -1,58 +1,61 @@
-const validateParams = params => {
-
-  // Download options
-  const MAPBOX_TOKEN = params.key || process.env.MAPBOX_TOKEN;
-  const tileset_id = commander.tileset;
-  const format = commander.format;
-
-  // Check for mapbox token
+/**
+ * Validate all the parameters passed in to mapboxed.
+ *
+ * Returns true if all parameters are valid, throws an Error if any parameter is invalid.
+ *
+ * Validation rules:
+ *  1 - Mapbox token must be valid
+ *  2 - format must be one of: jpg90, jpg80, jpg70, png256, png128, png64, png32, png
+ *  3 - zoom must be an integer larger than 0
+ *  4 - x values must be between -180 and 180, y values between -90 and 90.
+ *      x2 must be greater than x1, y2 must be less than y1.
+ *
+ * @param params
+ */
+const validateParams = (params) => {
+  // //////////////////////////////////////////////////////
+  // 1. Mapbox token must be valid.
+  const MAPBOX_TOKEN = params.key || (typeof process === 'object' && process.env.MAPBOX_TOKEN);
   if (!MAPBOX_TOKEN) {
-    console.error('No mapbox token found, please pass your mapbox API token via --key or set the MAPBOX_TOKEN environment variable.');
-    process.exit(1);
+    throw new Error(
+      'No mapbox token found, please pass your mapbox API token via --key or set the MAPBOX_TOKEN environment variable.',
+    );
   }
 
-  // Process format
-  if (['jpg', 'png'].indexOf(format.replace(/[0-9]/g, '')) < 0) {
-    console.error('format must be one of (jpg90 | jpg80 | jpg70 | png256 | png128 | png64 | png32 | png)');
-    process.exit(1);
+  // //////////////////////////////////////////////////////
+  // 2. format must be one of: jpg90, jpg80, jpg70, png256, png128, png64, png32, png
+  const validFormats = ['jpg90', 'jpg80', 'jpg70', 'png256', 'png128', 'png64', 'png32', 'png'];
+  if (validFormats.indexOf(params.format) < 0) {
+    throw new Error(`format must be one of (${validFormats.join(' | ')})`);
   }
 
-  // Process zoom value
-  let zoom;
-  try {
-    zoom = parseInt(commander.zoom);
-    if (zoom < 0) throw new Error();
-  } catch (e) {
-    console.error('zoom must be an integer greater than or equal to 0');
-    process.exit(1);
+  // //////////////////////////////////////////////////////
+  // 3. zoom must be an integer larger than 0
+  if (parseInt(params.zoom, 10) < 0) throw new Error('zoom must be an integer greater than or equal to 0');
+
+  // //////////////////////////////////////////////////////
+  // 4. x values must be between -180 and 180, y values between -90 and 90.
+  //    x2 must be greater than x1, y2 must be less than y1.
+  const layerBounds = [
+    parseFloat(params.X1), parseFloat(params.Y1), parseFloat(params.X2), parseFloat(params.Y2),
+  ];
+
+  if (
+    layerBounds[0] >= layerBounds[2] || layerBounds[1] <= layerBounds[3]
+    || layerBounds[0] > 180 || layerBounds[0] < -180
+    || layerBounds[2] > 180 || layerBounds[2] < -180
+    || layerBounds[1] > 90 || layerBounds[1] < -90
+    || layerBounds[3] > 90 || layerBounds[3] < -90
+  ) {
+    throw new Error(
+      `invalid latitude / longitude values. x values must be between -180 and 180, y values between -90 and 90. 
+      x2 must be greater than x1, y2 must be less than y1.`,
+    );
   }
 
-  // Process longitude / latitude values
-  let layerBounds;
-  try {
-    layerBounds = [
-      parseFloat(commander.X1),
-      parseFloat(commander.Y1),
-      parseFloat(commander.X2),
-      parseFloat(commander.Y2)
-    ];
-
-    if (
-      layerBounds[0] >= layerBounds[2]
-      || layerBounds[1] <= layerBounds[3]
-      || layerBounds[0] > 180
-      || layerBounds[0] < -180
-      || layerBounds[2] > 180
-      || layerBounds[2] < -180
-      || layerBounds[1] > 90
-      || layerBounds[1] < -90
-      || layerBounds[3] > 90
-      || layerBounds[3] < -90
-    ) throw new Error()
-  } catch (e) {
-    console.error('invalid latitude / longitude values. x values must be between -180 and 180, y values between -90 and 90. x2 must be greater than x1, y2 must be less than y1.');
-    process.exit(1)
-  }
+  // //////////////////////////////////////////////////////
+  // Return processed values
+  return true;
 };
 
 export default validateParams;
